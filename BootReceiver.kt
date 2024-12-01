e59ad699270011bdec8a06b69139f4509f761434
Imapo <83175739+Imapo@users.noreply.github.com>
2024-12-01 17:43:20 +0500

    Create BootReceiver.kt

---
+++ BootReceiver.kt
@@ -0,0 +1,19 @@
+package com.example.motivationalquotefortheday
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            Log.d("BootReceiver", "System boot completed, starting service...")
+
+            val activityIntent = Intent(context, MainActivity::class.java)
+            activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(activityIntent)
+        }
+    }
+}
